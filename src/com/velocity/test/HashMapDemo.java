ec6d758e634908ee281857422239f3b897fb1ed0
praveen <praveen.bhosle5@gmail.com>
2023-01-18 20:56:58 +0530

    New classes added

--- src/com/velocity/test/HashMapDemo.java
+++ src/com/velocity/test/HashMapDemo.java
@@ -1,10 +1,16 @@
 package com.velocity.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-
+		Map<Integer, String> map = new HashMap<Integer, String>();
+		map.put(101, "Praveen");
+		map.put(102, "naveen");
+		map.put(103, "raveen");
+		System.out.println(map);
 	}
 
 }
