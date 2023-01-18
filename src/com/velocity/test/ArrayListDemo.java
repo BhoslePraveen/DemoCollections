635e56ec141f4b63ed884eba759c73c2c6d4445e
praveen <praveen.bhosle5@gmail.com>
2023-01-18 21:21:00 +0530

    ArrayList class implementation

--- src/com/velocity/test/ArrayListDemo.java
+++ src/com/velocity/test/ArrayListDemo.java
@@ -1,9 +1,17 @@
 package com.velocity.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		List<String> list = new ArrayList<String>();
+		list.add("praveen");
+		list.add("sohan");
+		list.add("mohan");
+		System.out.println(list);
+		
 	}
 
 }
